e286237155c9433d4c237e0df2ba132a85123f4e
Edgar <edgarartur2000@mail.ru>
2025-11-13 16:02:34 +0400

    refactor:

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,41 +1,39 @@
-import { useTranslation } from 'react-i18next'
-import { SectionTitle } from '../../components/ui/SectionTitle/SectionTitle'
-import { ProjectList } from '../../components/ProjectList/ProjectList'
-import { Project } from '../../components/Project/Project'
-import { Frame, FrameTheme } from '../../components/ui/Frame/Frame'
-import { PROJECTS } from '../../constants/constants'
+import { useTranslation } from "react-i18next";
+import { Project } from "../../components/Project/Project";
+import { ProjectList } from "../../components/ProjectList/ProjectList";
+import { Frame, FrameTheme } from "../../components/ui/Frame/Frame";
+import { PROJECTS } from "../../constants/constants";
 
 const Projects = () => {
-  const { t } = useTranslation()
-  return (
-    <>
-      <div className="max-w-7xl mx-auto px-16 phone:px-4 mb-16">
-        <h1 className="text-3xl font-semibold text-white mb-3">
-          <span className="text-primary font-semibold">/</span>
-          {t('projects')}
-        </h1>
-        <span className='phone:text-2xl'>{t('List of my projects')}</span>
-      </div>
-      <section className="mb-20 relative">
-        <div className="max-w-7xl mx-auto px-16 phone:px-4">
+	const { t } = useTranslation();
+	return (
+		<>
+			<div className="max-w-7xl mx-auto px-16 phone:px-4 mb-16">
+				<h1 className="text-3xl font-semibold text-white mb-3">
+					<span className="text-primary font-semibold">/</span>
+					{t("projects")}
+				</h1>
+				<span className="phone:text-2xl">{t("List of my projects")}</span>
+			</div>
+			<section className="mb-20 relative">
+				<div className="max-w-7xl mx-auto px-16 phone:px-4">
+					<ProjectList>
+						{PROJECTS.map((project) => (
+							<Project key={project.title} project={project} />
+						))}
+					</ProjectList>
+				</div>
+				<Frame
+					theme={FrameTheme.GRID}
+					className="absolute -left-6 top-20 phone:hidden"
+				/>
+				<Frame
+					theme={FrameTheme.NORMAL}
+					className="absolute -right-14 -top-6 w-40 h-40 -z-10 tablet:hidden"
+				/>
+			</section>
+		</>
+	);
+};
 
-          <ProjectList>
-            {PROJECTS.map(project => (
-              <Project key={project.title} project={project} />
-            ))}
-          </ProjectList>
-        </div>
-        <Frame
-          theme={FrameTheme.GRID}
-          className="absolute -left-6 top-20 phone:hidden"
-        />
-        <Frame
-          theme={FrameTheme.NORMAL}
-          className="absolute -right-14 -top-6 w-40 h-40 -z-10 tablet:hidden"
-        />
-      </section>
-    </>
-  )
-}
-
-export default Projects
+export default Projects;
